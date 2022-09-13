aca99dbd26fb895b28f4ee808a17413f71ec76aa
emburyj <josh@LAPTOP-PBSFF0FB.localdomain>
2022-09-13 05:37:18 -0700

    added .env file, updated .gitignore, prepare settings for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 # import dj_database_url
@@ -21,11 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tmq4%4-ju1s3k11h0t2pz1@^n15+d1r^x@uy1@79cy-#j1!-29'
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
